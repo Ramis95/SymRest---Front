bb806209a28473a8ff50d00925192f476c78fc32
Ramis <i.ramis95@gmail.com>
2020-05-28 16:07:56 +0300

    'login-auth'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,18 +23,39 @@ export default {
   },
   mounted() {
     axios
-            .post('http://127.0.0.1:8000/api/login?email=test@test.ru&password=123123123')
+            // .post('http://127.0.0.1:8000/api/login?email=test@test.ru&password=123123123')
+            .get('http://127.0.0.1:8000/api/users?jwt=' + localStorage.getItem('jwt'))
+
             // .post('http://127.0.0.1:8000/api/logout')
             // .post('http://127.0.0.1:8000/api/profile')
             .then(response => {
+              console.log('ads');
               this.info = response;
               this.showInfo();
             })
   },
   methods: {
     showInfo() {
-      console.log(document.cookie);
-      console.log(this.info)
+      // console.log(document.cookie);
+
+      console.log(localStorage.getItem('jwt'));
+
+      if(this.info.data.result)
+      {
+          var jwt = this.info.data.params.jwt;
+          localStorage.setItem('jwt', jwt);
+
+          console.log(localStorage.getItem('jwt'));
+
+          // jwt = this.info.data.params.jwt;
+          // expireTime = this.info.data.params.expireTime;
+          //
+          // console.log(jwt);
+          // console.log(expireTime);
+
+          // console.log(params.jwt);
+      }
+
     }
   }
 }
